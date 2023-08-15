cecfec8a6802cd9834f68971361fc52e012cc8bd
Seonggoo Kang <ksung91889@naver.com>
2023-08-15 22:31:56 +0900

    사용자 상호작용 없다면 Server component로 다루라

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,13 @@
-export default function Read(props) {
+// 생각해야한다. 이페이지는 사용자와 상호작용 하는가?
+// 아니다. 데이터를 읽어서 출력할 뿐 >> 이런건 서버 컴퍼넌트로 가면됨
+export default async function Read(props) {
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await resp.json();
   return (
     <>
-      <h2>Read</h2>
-      parameters : {props.params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
+      {/* parameters : {props.params.id} */}
     </>
   );
 }
